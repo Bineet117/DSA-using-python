2997683bcc5f5737114d461ad2d694d6bb26ed2b
Bunny <bineetgupta117@gmail.com>
2025-09-23 01:00:47 +0530

    recursion

--- DSA_YT/8_recursion.py
+++ DSA_YT/8_recursion.py
@@ -1,22 +1,56 @@
 from logger_config import logger
 
 logger.info("taking input")
-n = int(input("give me some input: "))
+# n = int(input("give me some input: "))
+n = 4
 
-
+logger.info("--------------Head Recursion---------------------------")
 def head_recursion(n):
     if n == 0:
         return 
     head_recursion(n-1)
     print(n)
 
-head_recursion(n)
-logger.info("-----------------------------------------")
+# head_recursion(n)
 
+logger.info("--------------Tail Recursion---------------------------")
 def tail_recursion(n):
     if n == 0:
         return 
     print(n)
     tail_recursion(n-1)
 
-tail_recursion(n)
+# tail_recursion(n)
+
+
+logger.info("--------------Recursion using parameters---------------------------")
+# print x, n times
+x = 89
+n = 5
+def printing_data(x,n):
+    if n == 0:
+        return
+    print(x)
+    printing_data(x, n-1)
+
+# printing_data(x,n)
+
+logger.info("--------------Printing 1 to n---------------------------")
+n = 12
+def print_num(n):
+    if n == 0:
+        return
+    
+    print_num(n-1)
+    print(n)
+# print_num(n)
+
+n = 3
+def print_nnnn(k,n):
+    if k > n:
+        return
+    
+    print(k)
+    print_nnnn(k+1, n)
+
+print_nnnn(1,n)
