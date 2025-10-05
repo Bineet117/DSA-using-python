d00aa0c94b7c0b7772cc3f60fd0e361f30d47d48
Bunny <bineetgupta117@gmail.com>
2025-10-05 23:44:49 +0530

    merge

--- DSA_YT/15_merge_sort.py
+++ DSA_YT/15_merge_sort.py
@@ -1,3 +1,37 @@
 from logger_config import logger
 
-def 
+
+a = [2,8,15,18]
+b = [5,9,12,17]
+
+def merge_sorted_array(arr1 , arr2):
+    c = []
+    i = j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] <= arr2[j]:
+            c.append(arr1[i])
+            i = i+1
+        else:
+            c.append(arr2[j])
+            j = j+1
+    
+    c.extend(arr1[i:])
+    c.extend(arr2[j:])
+
+    return c
+
+# print(merge_sorted_array(a,b))
+
+
+
+def merge_sort(ar):
+    if len(ar) == 1:
+        return ar
+    
+    arr = len(ar)//2
+    left = merge_sort(ar[:arr])
+    right = merge_sort(ar[arr:])
+
+    return merge_sorted_array(left, right)
+
+print(merge_sort([34,21,9,0,2,4,5,7,11]))
