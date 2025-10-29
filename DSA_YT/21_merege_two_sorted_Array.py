ce9022f562ead01793df1fdbc8b9dbdbc5f0c82c
Bineet gupta <bineetgupta117@gmail.com>
2025-10-29 22:33:46 +0530

    find the missing element

--- DSA_YT/21_merege_two_sorted_Array.py
+++ DSA_YT/21_merege_two_sorted_Array.py
@@ -20,5 +20,27 @@ def sorted(num, num2):
 
 print(sorted(num, num2))
 
+
+num = [1,2,4,4,4,5,6]
+num2= [1,3,5,9]
+
+def sorted(num, num2):
+    lt = []
+    i = j = 0
+    while i < len(num) and j < len(num2):
+        if num[i] > num2[j]:
+            if num2[j] not in lt:
+                lt.append(num2[j])
+            j = j+1
+        else:
+            if num[i] not in lt:
+                lt.append(num[i])
+            i =i+1
+    lt.extend(num[i:])
+    lt.extend(num2[j:])
+
+    return lt # list(set(lt))
+
+print(sorted(num, num2))
     
     
---
+++ DSA_YT/22_find_the_missing_num.py
@@ -0,0 +1,11 @@
+from logger_config import logger 
+
+lt = [1,0,2,4]
+
+def missing(lt):
+    for i in range(len(lt) +1):
+        if i not in lt:
+            return i
+    return -1
+
+print(missing(lt))
