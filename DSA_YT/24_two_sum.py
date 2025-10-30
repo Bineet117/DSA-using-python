a7a6329bb2a343fcabe42cb0170157454ef73a5c
Bineet gupta <bineetgupta117@gmail.com>
2025-10-30 13:44:04 +0530

    two sum

--- DSA_YT/24_two_sum.py
+++ DSA_YT/24_two_sum.py
@@ -1,3 +1,7 @@
+from logger_config import logger
+
+logger.info("brute force")
+
 def two_sum(lst, num):
     for i in range(len(lst)):
         for j in range(0,len(lst)):
@@ -10,3 +14,25 @@ def two_sum(lst, num):
 
 
 print(two_sum([5,9,1,2,4,15,6,3], 13))
+
+# TC: O(n)
+# SC: O(n)
+
+
+logger.info("optimal way ")
+
+def two_sum(lst, num):
+    dic = {}
+    for i in range(len(lst)):
+        remaining = num-lst[i]
+
+        if remaining in dic:
+            return i, dic[remaining]
+        dic[lst[i]] = i
+    return -1
+    
+
+print(two_sum([5,9,1,2,4,15,6,3], 13))
+
+# TC: O(n) 
+# SC: O(n)
